bc14f71bcf536282055aecca37c3ef4be3ef3e60
ejkim <kim9912@hanmail.net>
2021-10-18 21:24:24 +0900

    re_9663
    Understanding logic of 'col' list

--- 문제풀이/14.백트래킹/**9663.py
+++ 문제풀이/14.백트래킹/**9663.py
@@ -1,49 +1,50 @@
 # 체스에서 퀸은 가로, 세로, 대각선 방향으로 끝까지 이동할 수 있다.
 # 즉, N개의 퀸을 체스판 위에 올려놓았을 때, 서로 가로, 세로, 대각선 방향으로 겹치면 안된다.
 
-# 퀸들이 서로 공격하지 못하게 하기 위해서는 같은 줄(가로줄이던, 세로줄이던)에 있을 수 없는데, 이를 이용할 수 있었다.
-# 요소의 개수가 N인 list를 이용하여 각 요소는 해당 열의 퀸의 위치를 표시하도록 하였다. (퀸은 한개의 열에 한개만 있을 수 있으므로) ==> visited
-# 또한 해당 행에 퀸이 존재하는지를 표시하는 list를 이용하고, 서로 대각선 방향으로 겹치는지 체크하는 함수를 이용하였다.
 
 '''
 퀸을 한개씩 추가로 놓을 때마다 해당 위치에 퀸이 위치할 수 있는지를 체크하고,
 놓을 수 있으면 놓고 다음 퀸의 위치를 탐색한다.
 N*N 체스판에 N개의 퀸 놓기를 성공하면 count를 한개 추가하고 탐색을 계속한다.
+
+1. 같은 열인지 확인
+· col[i] : i 번째 행에서 퀸이 놓여있는 열의 위치
+· col[k] : k 번째 행에서 퀸이 놓여있는 열의 위치
+------> col[i]==col[k] 일때 같은 열에 놓이게 되므로 유망하지 않음!
+
+2. 대각선 체크
+| col[i] - col[k] | = i - k 인 경우 유망하지 않다.
+
 '''
 
 
 import sys
 
-n = int(sys.stdin.readline())
-board = [0] * n
-ans_count = 0
-visited = [False] * n  # n번째 열에 퀸이 있는지를 표시하는 list
+# 1. 퀸의 개수 n을 받아오고 퀸의 행의 위치를 저장할 col리스트를 생성한다.
+N = int(sys.stdin.readline())
+count = 0
+col = [0] * N
 
-def check(x):   # 기존에 있는 퀸들과 대각선 방향으로 겹치는 퀸이 있는지를 확인
-    for i in range(x):
-        if abs(board[x] - board[i]) == x - i:
-        # 값이 0이라면 퀸이 자리에 없는 의미(처음에 0으로 초기화 해놨기 때문)에
-        # => 퀸이
+# 2. 퀸의 위치가 올바른지 확인할 함수를 생성한다.
+def isCorrect(i):
+    for x in range(i):
+        if col[i] == col[x] or abs(col[i] - col[x] ) == i-x:
+        # 만약 같은 열에 위치하거나 대각선에 위치한다면
             return False
+            #올바른 위치가 아니다.
     return True
 
-def n_queen(x):     # dfs
-    global ans_count
-    if x == n:
-        ans_count += 1
-        return
-    for i in range(n):
-        if visited[i]:  # i번째 열에 퀸이 있을 경우 continue
-            continue
+# 3. i는 0부터 시작해 n이 되면 경우의 수 + 1을 해준다
+def nQueens(i):
+    global  count
+    if i == N:      # 만약 퀸의 개수가 n개라면 탈출하면서 경우의 수를 반환
+        count += 1
+    else:
+        for j in range(N):
+            col[i] = j  #col[1] = 1-n 까지 1, 1~1, n 까지 반복
+            if isCorrect(i):
+                nQueens(i + 1)
 
-        board[x] = i
-        if check(x):
-            visited[i] = True
-            n_queen(x+1)
-            visited[i] = False
-
-n_queen(0)
-print(ans_count)
-
-
-
+# 4. nQueens는 0부터 시작하여 count를 반환한다.
+nQueens(0)
+print(count)
