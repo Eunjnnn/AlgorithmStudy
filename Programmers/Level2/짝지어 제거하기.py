5ecd0a6936b3a418a3b29f6ddc17267cac285ede
Eunjnnn <kim9912@hanmail.net>
2022-02-12 20:25:49 +0900

    짝지어 제거하기

---
+++ Programmers/Level2/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+## stack에 넣으면서 비교. 같으면 pop
+def solution(s):
+    answer = 0
+    first = 0
+    stack = []
+    
+    for i in s:
+        if len(stack) == 0: 
+            stack.append(i)
+        elif stack[-1] == i: 
+            stack.pop()
+        else:
+            stack.append(i)
+        
+    if stack:
+        return 0
+    else: return 1
